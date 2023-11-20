48ef8c8a1487c9cebc41bf0050006abb9fbb1711
xTirian <tirianx@yahoo.com>
2023-11-19 22:17:52 -0300

    feat: add cadastro form useStates

--- src/Components/CadastroFrom/index.tsx
+++ src/Components/CadastroFrom/index.tsx
@@ -1,24 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 const index = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
 
-  const cadastrar = (e:any) => {
-    e.preventDefault()
-  }
+  const cadastrar = (e: any) => {
+    e.preventDefault();
+    //TODO send to server
+  };
+
+  const alterarValores = (e: { target: { name: any; value: any } }) => {
+    const { name, value } = e.target;
+
+    console.log(name,value)
+
+    switch (name) {
+      case 'nameInput':
+        setNome(value);
+        break;
+
+      case 'emailInput':
+        setEmail(value);
+        break;
+
+      case 'passwordInput':
+        setSenha(value);
+        break;
+
+      default:
+        break;
+    }
+
+    
+  };
 
   return (
     <form onSubmit={cadastrar}>
       <div>
         <label htmlFor="nameInput">Nome</label>
-        <input type="text" name="name" id="nameInput" />
+        <input
+          type="text"
+          name="nameInput"
+          id="nameInput"
+          onChange={alterarValores}
+        />
       </div>
       <div>
         <label htmlFor="emailInput">E-mail</label>
-        <input type="text" name="email" id="emailInput" />
+        <input
+          type="text"
+          name="emailInput"
+          id="emailInput"
+          onChange={alterarValores}
+        />
       </div>
       <div>
         <label htmlFor="passwordInput">Senha</label>
-        <input type="text" name="password" id="passwordInput" />
+        <input
+          type="senha"
+          name="passwordInput"
+          id="passwordInput"
+          onChange={alterarValores}
+        />
       </div>
       <button type="submit">Enviar</button>
     </form>
